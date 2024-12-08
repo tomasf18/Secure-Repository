0952a3ce94a5866cb45d77f46780ca055a675e35
DaniloMicael <danilomc2004@gmail.com>
2024-12-08 12:12:41 +0000

    refactor pytest

--- delivery2/src/tests/test.py
+++ delivery2/src/tests/test.py
@@ -3,6 +3,7 @@ import time
 import pytest
 import os
 import subprocess
+import json
 
 CONFIG = {
     "current_dir": os.getcwd(),
@@ -10,6 +11,9 @@ CONFIG = {
     "server_path": os.path.abspath("server/server.py"),
     "commands_dir": os.path.abspath("client/commands"),
 }
+
+with open("tests/data.json", "r") as f:
+    data = json.load(f)
 
 # NOTE: The scope "session" runs the fixture only once for the entire test session.
 # Use "function" scope to execute the fixture before each individual test.
@@ -76,6 +80,26 @@ def start_server():
         
 # ================== Helper Functions ==================
 
+def execute_and_validate(command, args, expected_stdout, expected_stderr="", print_message=None):
+    """
+    Execute a command and validate its output.
+
+    :param command: Name of the command to run (e.g., 'rep_subject_credentials').
+    :param args: List of arguments to pass to the command.
+    :param expected_stdout: Expected string or substring in the stdout.
+    :param expected_stderr: Expected string or substring in the stderr (default is empty).
+    :param print_message: Optional message to print before running the command.
+    """
+    if print_message:
+        print(f"\n{print_message}")
+    
+    stdout, stderr = run_command(command, *args)
+    
+    assert expected_stdout in stdout, f"Expected '{expected_stdout}' in stdout, but got: {stdout}"
+    assert expected_stderr in stderr, f"Expected '{expected_stderr}' in stderr, but got: {stderr}"
+    
+    print(stdout)
+
 def run_command(command, *args):
     """
     Helper function to run a shell command with arguments and capture its output.
@@ -107,96 +131,137 @@ def test_simple():
     """A simple test to add some data to the database."""
     print("\n======================== Testing simple commands ========================\n")
     
-    # Create user1 credentials
-    print(f"\nCreating user1 credentials...")
-    stdout, stderr = run_command("rep_subject_credentials", "123", "user1_cred_file")
-    assert "Private key saved to ../keys/subject_keys/priv_user1_cred_file.pem" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    subjects = data["subjects"]
+    subjects_dict = {subject["username"]: subject for subject in subjects}
+    organizations = data["organizations"]
+    sessions = data["sessions"]
     
-    # Create an organization with the user1
-    print(f"\nCreating an organization with the user1...")
-    stdout, stderr = run_command("rep_create_org", "org1", "user1", "User1", "user1@gmail.com", "user1_cred_file")
-    assert "Organization org1 created successfully" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    
+    # ----------------- Add Subject Credentials -----------------
+    
+    for subject in subjects:
+        username = subject["username"]
+        password = subject["password"]
+        cred_file = subject["public_key"]
+        
+        # Create subject credentials
+        execute_and_validate(
+            command="rep_subject_credentials",
+            args=[password, cred_file],
+            expected_stdout=f"Private key saved to ../keys/subject_keys/priv_{cred_file}.pem",
+            print_message=f"Creating {username} credentials..."
+        )
+    
+    # ----------------- Create Organizations -----------------
+    
+    for org in organizations:
+        org_name = org["org_name"]
+        owner_username = org["owner_username"]
+        
+        # Get the owner's data
+        owner = subjects_dict[owner_username]
+        full_name = owner["full_name"]
+        email = owner["email"]
+        cred_file = owner["public_key"]
+        
+        # Create organizations
+        execute_and_validate(
+            command="rep_create_org",
+            args=[org_name, owner_username, full_name, email, cred_file],
+            expected_stdout=f"Organization {org_name} created successfully",
+            print_message=f"Creating organization {org_name} with owner {owner_username}..."
+        )
     
     # List organizations
-    print(f"\nListing organizations...")
-    stdout, stderr = run_command("rep_list_orgs")
-    assert "[{'name': 'org1'}]" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    execute_and_validate(
+        command="rep_list_orgs",
+        args=[],
+        expected_stdout="[{'name': 'org1'}, {'name': 'org2'}]",
+        print_message="Listing organizations..."
+    )
     
-    # Create a session with the user1
-    print(f"\nCreating a session with the user1...")
-    stdout, stderr = run_command("rep_create_session", "org1", "user1", "123", "user1_cred_file", "user1_session_file")
-    assert "Session created and saved to ../sessions/user1_session_file.json, sessionId=1" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    # ----------------- Create Sessions -----------------
+    target_subjects = {"user1", "user6"}
     
-    # List subjects
-    print(f"\nListing subjects...")
-    stdout, stderr = run_command("rep_list_subjects", "user1_session_file")
-    assert "[{'username': 'user1', 'status': 'ACTIVE'}]" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    for session in sessions:
+        if session["session_user"] in target_subjects:
+            session_org = session["session_org"]
+            session_user = session["session_user"]
+            session_file = session["session_file"]
+            session_id = session["session_id"]
+            
+            # Get the session user's data
+            subject = subjects_dict[session_user]
+            password = subject["password"]
+            cred_file = subject["public_key"]
+
+            # Create sessions
+            execute_and_validate(
+                command="rep_create_session",
+                args=[session_org, session_user, password, cred_file, session_file],
+                expected_stdout=f"Session created and saved to ../sessions/{session_file}.json, sessionId={session_id}",
+                print_message=f"Creating a session with {session_user}..."
+            )
     
-    # Create user2 credentials
-    print(f"\nCreating user2 credentials...")
-    stdout, stderr = run_command("rep_subject_credentials", "456", "user2_cred_file")
-    assert "Private key saved to ../keys/subject_keys/priv_user2_cred_file.pem" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    # ----------------- Add subjects to organizations -----------------
+    org1_target_subjects = {"user2", "user3", "user4", "user5"}
+    user1_org1_session_file = sessions[0]["session_file"]
+    org2_target_subjects = {"user7", "user8", "user9", "user10"}
+    user6_org2_session_file = sessions[5]["session_file"]
     
-    # Add user2 to the organization
-    print(f"\nAdding user2 to the organization org1...")
-    stdout, stderr = run_command("rep_add_subject", "user1_session_file", "user2", "User2", "user2@gmail.com", "user2_cred_file")
-    assert "Subject user2 added to organization org1 successfully" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    for subject in org1_target_subjects:
+        # Get the subject's data
+        subject_data = subjects_dict[subject]
+        username = subject_data["username"]
+        full_name = subject_data["full_name"]
+        email = subject_data["email"]
+        cred_file = subject_data["public_key"]
+        
+        # Add subjects to org1
+        execute_and_validate(
+            command="rep_add_subject",
+            args=[user1_org1_session_file, username, full_name, email, cred_file],
+            expected_stdout=f"Subject {username} added to organization org1 successfully",
+            print_message=f"Adding {username} to org1..."
+        )
     
-    # Create user3 credentials
-    print(f"\nCreating user3 credentials...")
-    stdout, stderr = run_command("rep_subject_credentials", "789", "user3_cred_file")
-    assert "Private key saved to ../keys/subject_keys/priv_user3_cred_file.pem" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    for subject in org2_target_subjects:
+        # Get the subject's data
+        subject_data = subjects_dict[subject]
+        username = subject_data["username"]
+        full_name = subject_data["full_name"]
+        email = subject_data["email"]
+        cred_file = subject_data["public_key"]
+        
+        # Add subjects to org
+        execute_and_validate(
+            command="rep_add_subject",
+            args=[user6_org2_session_file, username, full_name, email, cred_file],
+            expected_stdout=f"Subject {username} added to organization org2 successfully",
+            print_message=f"Adding {username} to org2..."
+        )
     
-    # Add user3 to the organization
-    print(f"\nAdding user3 to the organization org1...")
-    stdout, stderr = run_command("rep_add_subject", "user1_session_file", "user3", "User3", "user3@gmail.com", "user3_cred_file")
-    assert "Subject user3 added to organization org1 successfully" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    # ----------------- List subjects -----------------
+    execute_and_validate(
+        command="rep_list_subjects",
+        args=[user1_org1_session_file],
+        expected_stdout="[{'username': 'user1', 'status': 'ACTIVE'}, {'username': 'user2', 'status': 'ACTIVE'}, {'username': 'user3', 'status': 'ACTIVE'}, {'username': 'user4', 'status': 'ACTIVE'}, {'username': 'user5', 'status': 'ACTIVE'}]",
+        print_message="Listing subjects for org1..."
+    )
     
-    # Create user4 credentials
-    print(f"\nCreating user4 credentials...")
-    stdout, stderr = run_command("rep_subject_credentials", "101112", "user4_cred_file")
-    assert "Private key saved to ../keys/subject_keys/priv_user4_cred_file.pem" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    execute_and_validate(
+        command="rep_list_subjects",
+        args=[user1_org1_session_file, "user3"],
+        expected_stdout="{'username': 'user3', 'status': 'ACTIVE'}",
+        print_message="Listing subject user3..."
+    )
     
-    # Add user4 to the organization
-    print(f"\nAdding user4 to the organization org1...")
-    stdout, stderr = run_command("rep_add_subject", "user1_session_file", "user4", "User4", "user4@gmail.com", "user4_cred_file")
-    assert "Subject user4 added to organization org1 successfully" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
-    
-    # List subjects
-    print(f"\nListing subjects...")
-    stdout, stderr = run_command("rep_list_subjects", "user1_session_file")
-    assert "[{'username': 'user1', 'status': 'ACTIVE'}, {'username': 'user2', 'status': 'ACTIVE'}, {'username': 'user3', 'status': 'ACTIVE'}, {'username': 'user4', 'status': 'ACTIVE'}]" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
-    
-    # List subject user2
-    print(f"\nListing subject user2...")
-    stdout, stderr = run_command("rep_list_subjects", "user1_session_file", "user2")
-    assert "{'username': 'user2', 'status': 'ACTIVE'}" in stdout
-    assert stderr == ""  # No errors expected
-    print(stdout)
+    execute_and_validate(
+        command="rep_list_subjects",
+        args=[user6_org2_session_file],
+        expected_stdout="[{'username': 'user10', 'status': 'ACTIVE'}, {'username': 'user6', 'status': 'ACTIVE'}, {'username': 'user7', 'status': 'ACTIVE'}, {'username': 'user8', 'status': 'ACTIVE'}, {'username': 'user9', 'status': 'ACTIVE'}]",
+        print_message="Listing subjects for org2..."
+    )
 
 def test_subjects():
     """A simple test to check if the subject commands work."""
@@ -206,158 +271,3 @@ def test_subjects():
 def test_docs():
     """A simple test to check if the document commands work."""
     print("\n======================== Testing documents ========================\n")
-      
-
-# ./rep_subject_credentials 123 user1_cred_file
-# ./rep_create_org org1 user1 User1 user1@gmail.com user1_cred_file
-# ./rep_list_orgs
-# ./rep_create_session org1 user1 123 user1_cred_file user1_session_file
-# ./rep_list_subjects user1_session_file
-
-# ./rep_subject_credentials 456 user2_cred_file
-# ./rep_add_subject user1_session_file user2 User2 user2@gmail.com user2_cred_file
-
-# ./rep_subject_credentials 789 user3_cred_file
-# ./rep_add_subject user1_session_file user3 User3 user3@gmail.com user3_cred_file
-
-# ./rep_subject_credentials 101112 user4_cred_file
-# ./rep_add_subject user1_session_file user4 User4 user4@gmail.com user4_cred_file
-
-# ./rep_list_subjects user1_session_file
-# ./rep_list_subjects user1_session_file user2
-
-# ---
-
-# ./rep_create_session org1 user2 456 user2_cred_file user2_session_file
-# ./rep_suspend_subject user2_session_file user4
-# ./rep_list_subjects user2_session_file user4
-
-# --- 
-
-# ./rep_create_session org1 user3 789 user3_cred_file user3_session_file
-# ./rep_activate_subject user3_session_file user4
-# ./rep_list_subjects user3_session_file user4
-
-# ---
-
-# ./rep_add_doc user1_session_file doc1 file1.txt
-
-# def test_create_org():
-#     orgname1='org1'
-#     pub_key = 'pub_key.pem'
-
-#     ret = os.system(f'./rep_create_org {orgname1} joao Joao joao@gmail.com {pub_key}')
-#     assert ret == 0
-
-# def test_list_orgs():
-#     orgname2='org2'
-#     pub_key = 'pub_key.pem'
-
-#     os.system(f'./rep_create_org {orgname2} danilo Danilo danilo@gmail.com {pub_key}')
-#     output = subprocess.check_output(f'./rep_list_orgs', shell=True, text=True)
-#     assert "{'name': 'org1'}, {'name': 'org2'}" in output
-
-# def test_create_session():
-#     orgname='org1'
-#     session_file = 'session1.json'
-#     priv_key = 'key.pem'
-#     priv_key_password = 123
-
-#     ret = os.system(f'./rep_create_session {orgname} joao {priv_key_password} {priv_key} {session_file}')
-#     assert ret == 0
-
-# def test_list_subjects():
-#     session_file = 'session1.json'
-
-#     ret = os.system(f'./rep_list_subjects {session_file}')
-#     assert ret == 0
-#     output = subprocess.check_output(f'./rep_list_subjects {session_file}', shell=True, text=True)
-#     assert "{'username': 'joao', 'status': 'ACTIVE'}" in output
-
-# def test_add_subject():
-#     session_file = 'session1.json'
-#     pub_key = 'pub_key.pem'
-
-#     ret = os.system(f'./rep_add_subject {session_file} pedro Pedro pedro@gmail.com {pub_key}')
-#     assert ret == 0
-#     output = subprocess.check_output(f'./rep_list_subjects {session_file}', shell=True, text=True)
-#     assert "{'username': 'pedro', 'status': 'ACTIVE'}" in output
-
-# def test_suspend_subject():
-#     session_file = 'session1.json'
-
-#     ret = os.system(f'./rep_suspend_subject {session_file} pedro')
-#     assert ret == 0
-#     output = subprocess.check_output(f'./rep_list_subjects {session_file}', shell=True, text=True)
-#     assert "{'username': 'pedro', 'status': 'SUSPENDED'}" in output
-
-# def test_activate_subject():
-#     session_file = 'session1.json'
-
-#     ret = os.system(f'./rep_activate_subject {session_file} pedro')
-#     assert ret == 0
-#     output = subprocess.check_output(f'./rep_list_subjects {session_file}', shell=True, text=True)
-#     assert "{'username': 'pedro', 'status': 'ACTIVE'}" in output
-    
-# def test_add_doc():
-#     session_file = 'session1.json'
-#     doc_name = 'doc1'
-#     file = 'file_contents.txt'
-    
-#     ret = os.system(f'./rep_add_doc {session_file} {doc_name} {file}')
-#     assert ret == 0
-    
-# def test_list_docs():
-#     session_file = 'session1.json'
-    
-#     ret = os.system(f'./rep_list_docs {session_file}')
-#     assert ret == 0
-#     output = subprocess.check_output(f'./rep_list_docs {session_file}', shell=True, text=True)
-#     assert "{'document_name': 'doc1'}" in output
-    
-#     session_file2 = 'session2.json'
-#     os.system(f'./rep_create_session org2 danilo 123 key.pem {session_file2}')
-    
-#     ret = os.system(f'./rep_add_doc {session_file2} doc2 file_contents.txt')
-#     assert ret == 0
-#     output = subprocess.check_output(f'./rep_list_docs {session_file}', shell=True, text=True)
-#     assert "{'document_name': 'doc2'}" not in output
-
-# def test_get_doc_metadata():
-#     session_file = 'session1.json'
-#     doc_name = 'doc1'
-    
-#     ret = os.system(f'./rep_get_doc_metadata {session_file} {doc_name}')
-#     assert ret == 0
-#     output = subprocess.check_output(f'./rep_get_doc_metadata {session_file} {doc_name}', shell=True, text=True)
-#     print("\n\nOUTPUT = ", output)
-#     assert "'document_name': 'doc1'" in output
-    
-# def test_get_doc_file():
-#     session_file = 'session1.json'
-#     doc_name = 'doc1'
-    
-#     ret = os.system(f'./rep_get_doc_file {session_file} {doc_name}')
-#     assert ret == 0
-#     output = subprocess.check_output(f'./rep_get_doc_file {session_file} {doc_name}', shell=True, text=True)
-#     assert "Message to encrypt" in output
-
-# def test_delete_doc():
-#     session_file = 'session1.json'
-#     doc_name = 'doc1'
-    
-#     ret = os.system(f'./rep_delete_doc {session_file} {doc_name}')
-#     assert ret == 0
-
-#     try:
-#         output = subprocess.check_output(
-#             f'./rep_get_doc_file {session_file} {doc_name}', 
-#             shell=True, 
-#             text=True, 
-#             stderr=subprocess.STDOUT
-#         )
-#     except subprocess.CalledProcessError as e:
-#         output = e.output
-
-#     assert "404" in output
-#     assert "Document 'doc1' does not have an associated file handle" in output
