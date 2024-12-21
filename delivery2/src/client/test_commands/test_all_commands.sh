56b17267fcd746d592f0f6f5762cb47b3601390a
DaniloMicael <danilomc2004@gmail.com>
2024-12-21 17:13:39 +0000

    test all comands

--- delivery2/src/client/test_commands/test_all_commands.sh
+++ delivery2/src/client/test_commands/test_all_commands.sh
@@ -4,351 +4,140 @@ cd ../commands
 # Add subjects credentials
 ./rep_subject_credentials 123 user1_cred_file
 ./rep_subject_credentials 456 user2_cred_file
-./rep_subject_credentials 789 user3_cred_file
-./rep_subject_credentials 321 user4_cred_file
-./rep_subject_credentials 654 user5_cred_file
-./rep_subject_credentials 987 user6_cred_file
-./rep_subject_credentials 213 user7_cred_file
-./rep_subject_credentials 645 user8_cred_file
-./rep_subject_credentials 978 user9_cred_file
-./rep_subject_credentials 312 user10_cred_file
 
-
-# Create organizations
+# Create organization
 ./rep_create_org org1 user1 User1 user1@gmail.com user1_cred_file
-./rep_create_org org2 user6 User6 user6@gmail.com user6_cred_file
 
+# Create sessions
+./rep_create_session org1 user1 123 user1_cred_file user1_org1_session_file
+
+# Add subjects (without permission)
+./rep_add_subject user1_org1_session_file user2 User2 user2@gmail.com user2_cred_file # Should fail
+
+# Assume role (Manager)
+./rep_assume_role user1_org1_session_file Manager
+
+# Add subjects (with permission)
+./rep_add_subject user1_org1_session_file user2 User2 user2@gmail.com user2_cred_file
+
+# Add roles and permissions
+./rep_add_role user1_org1_session_file ROLE_1
+./rep_add_role user1_org1_session_file ROLE_2
+./rep_add_permission user1_org1_session_file ROLE_2 DOC_NEW
 
 # List organizations
 ./rep_list_orgs
 
+# Add user1 to ROLE_1
+./rep_add_permission user1_org1_session_file ROLE_1 user1
 
-# Create sessions
-./rep_create_session org1 user1 123 user1_cred_file user1_org1_session_file
-./rep_create_session org2 user6 987 user6_cred_file user6_org2_session_file
-
-
-# Add subjects to organizations
-./rep_add_subject user1_org1_session_file user2 User2 user2@gmail.com user2_cred_file
-./rep_add_subject user1_org1_session_file user3 User3 user3@gmail.com user3_cred_file
-./rep_add_subject user1_org1_session_file user4 User4 user4@gmail.com user4_cred_file
-./rep_add_subject user1_org1_session_file user5 User5 user5@gmail.com user5_cred_file
-
-./rep_add_subject user6_org2_session_file user7 User7 user7@gmail.com user7_cred_file
-./rep_add_subject user6_org2_session_file user8 User8 user8@gmail.com user8_cred_file
-./rep_add_subject user6_org2_session_file user9 User9 user9@gmail.com user9_cred_file
-./rep_add_subject user6_org2_session_file user10 User10 user10@gmail.com user10_cred_file
-
-
-# List subjects
-./rep_list_subjects user1_org1_session_file
-./rep_list_subjects user1_org1_session_file user3
-./rep_list_subjects user6_org2_session_file
-
-
-# Create sessions for the new subjects
-./rep_create_session org1 user2 456 user2_cred_file user2_org1_session_file
-./rep_create_session org1 user3 789 user3_cred_file user3_org1_session_file
-./rep_create_session org1 user4 321 user4_cred_file user4_org1_session_file
-./rep_create_session org1 user5 654 user5_cred_file user5_org1_session_file
-
-./rep_create_session org2 user7 213 user7_cred_file user7_org2_session_file
-./rep_create_session org2 user8 645 user8_cred_file user8_org2_session_file
-./rep_create_session org2 user9 978 user9_cred_file user9_org2_session_file
-./rep_create_session org2 user10 312 user10_cred_file user10_org2_session_file
-
-
-# Suspend 2 subjects
-./rep_suspend_subject user1_org1_session_file user2
-./rep_suspend_subject user7_org2_session_file user10
-
-
-# List subjects
-./rep_list_subjects user1_org1_session_file user2
-./rep_list_subjects user6_org2_session_file user10
-
-
-# Activate 1 subject
-./rep_activate_subject user1_org1_session_file user2
-
-
-# List subjects
-./rep_list_subjects user1_org1_session_file 
-./rep_list_subjects user6_org2_session_file
-
-
-# Add documents
-./rep_add_doc user1_org1_session_file doc1 file1.txt
-./rep_add_doc user1_org1_session_file doc2 file2.txt
-./rep_add_doc user1_org1_session_file doc3 file3.txt
-./rep_add_doc user1_org1_session_file doc4 file4.txt
-./rep_add_doc user1_org1_session_file doc5 file5.txt
-
-./rep_add_doc user6_org2_session_file doc6 file6.txt
-./rep_add_doc user6_org2_session_file doc7 file7.txt
-./rep_add_doc user6_org2_session_file doc8 file8.txt
-./rep_add_doc user6_org2_session_file doc9 file9.txt
-./rep_add_doc user6_org2_session_file doc10 file10.txt
-
-
-# Get decrypted files
-./rep_get_doc_file user1_org1_session_file doc1
-./rep_get_doc_file user1_org1_session_file doc2
-./rep_get_doc_file user1_org1_session_file doc3
-
-./rep_get_doc_file user6_org2_session_file doc6
-./rep_get_doc_file user6_org2_session_file doc7
-./rep_get_doc_file user6_org2_session_file doc8
-
-
-# Delete documents and get their metadata to check if they were deleted
-./rep_delete_doc user1_org1_session_file doc4
-./rep_get_doc_metadata user1_org1_session_file doc4
-
-./rep_delete_doc user6_org2_session_file doc9
-./rep_get_doc_metadata user6_org2_session_file doc9
-
-
-# Add roles
-./rep_add_role user1_org1_session_file ROLE_1
-./rep_add_role user1_org1_session_file ROLE_2
-./rep_add_role user1_org1_session_file ROLE_3
-
-./rep_add_role user6_org2_session_file ROLE_4
-./rep_add_role user6_org2_session_file ROLE_5
-./rep_add_role user6_org2_session_file ROLE_6
-
-
-# Assume roles
+# Assume and drop roles
 ./rep_assume_role user1_org1_session_file ROLE_1
-./rep_assume_role user1_org1_session_file ROLE_2
-./rep_assume_role user1_org1_session_file ROLE_3
-./rep_assume_role user2_org1_session_file ROLE_1
-./rep_assume_role user2_org1_session_file ROLE_2
-
-./rep_assume_role user2_org1_session_file ROLE_3 # To drop
-
-./rep_assume_role user3_org1_session_file ROLE_3
-./rep_assume_role user3_org1_session_file ROLE_1
-./rep_assume_role user4_org1_session_file ROLE_2
-
-
-./rep_assume_role user6_org2_session_file ROLE_4
-./rep_assume_role user6_org2_session_file ROLE_5
-./rep_assume_role user6_org2_session_file ROLE_6
-./rep_assume_role user7_org2_session_file ROLE_4
-./rep_assume_role user7_org2_session_file ROLE_5
-
-./rep_assume_role user7_org2_session_file ROLE_6 # To drop
-
-./rep_assume_role user8_org2_session_file ROLE_6
-./rep_assume_role user8_org2_session_file ROLE_4
-./rep_assume_role user9_org2_session_file ROLE_5
-
+./rep_drop_role user1_org1_session_file ROLE_1
 
 # List session roles
 ./rep_list_roles user1_org1_session_file
-./rep_list_roles user2_org1_session_file
-./rep_list_roles user3_org1_session_file
-./rep_list_roles user4_org1_session_file
 
-./rep_list_roles user6_org2_session_file
-./rep_list_roles user7_org2_session_file
-./rep_list_roles user8_org2_session_file
-./rep_list_roles user9_org2_session_file
+# User 2 creates session
+./rep_create_session org1 user2 456 user2_cred_file user2_org1_session_file
 
+# Assume role (not bound to)
+./rep_assume_role user2_org1_session_file ROLE_2    # Should fail
 
-# Drop roles
-./rep_drop_role user2_org1_session_file ROLE_3
-./rep_drop_role user7_org2_session_file ROLE_6
+# Add user 2 to ROLE_2
+./rep_add_permission user1_org1_session_file ROLE_2 user2
+./rep_assume_role user2_org1_session_file ROLE_2
 
+# List user2 roles on org1
+./rep_list_subject_roles user1_org1_session_file user2
 
-# List seession roles
-./rep_list_roles user1_org1_session_file
-./rep_list_roles user2_org1_session_file
-./rep_list_roles user3_org1_session_file
-./rep_list_roles user4_org1_session_file
-
-./rep_list_roles user6_org2_session_file
-./rep_list_roles user7_org2_session_file
-./rep_list_roles user8_org2_session_file
-./rep_list_roles user9_org2_session_file
-
-# User 1: [ROLE_1, ROLE_2, ROLE_3]
-# User 2: [ROLE_1, ROLE_2]
-# User 3: [ROLE_1, ROLE_3]
-# User 4: [ROLE_2]
-
-# User 6: [ROLE_4, ROLE_5, ROLE_6]
-# User 7: [ROLE_4, ROLE_5]
-# User 8: [ROLE_4, ROLE_6]
-# User 9: [ROLE_5]
-
+# List subjects on org1
+./rep_list_subjects user1_org1_session_file
 
 # List subjects who have a role
 ./rep_list_role_subjects user1_org1_session_file ROLE_1
-./rep_list_role_subjects user1_org1_session_file ROLE_2
-./rep_list_role_subjects user1_org1_session_file ROLE_3
 
-./rep_list_role_subjects user6_org2_session_file ROLE_4
-./rep_list_role_subjects user6_org2_session_file ROLE_5
-./rep_list_role_subjects user6_org2_session_file ROLE_6
-
-
-# List roles of a subject
-./rep_list_subject_roles user1_org1_session_file user1
-./rep_list_subject_roles user1_org1_session_file user2
-./rep_list_subject_roles user1_org1_session_file user3
-./rep_list_subject_roles user1_org1_session_file user4
-
-./rep_list_subject_roles user6_org2_session_file user6
-./rep_list_subject_roles user6_org2_session_file user7
-./rep_list_subject_roles user6_org2_session_file user8
-./rep_list_subject_roles user6_org2_session_file user9
-
-
-# Suspend roles
-./rep_suspend_role user2_org1_session_file ROLE_3
-./rep_suspend_role user9_org2_session_file ROLE_4
-
-
-# Reactivate roles
-./rep_reactivate_role user2_org1_session_file ROLE_3
-./rep_reactivate_role user9_org2_session_file ROLE_4
-
-
-# List role permissions
-./rep_list_role_permissions user2_org1_session_file Manager
-./rep_list_role_permissions user2_org1_session_file ROLE_2
-./rep_list_role_permissions user9_org2_session_file ROLE_5
-
-
-# Add permissions to roles
-./rep_add_permission user2_org1_session_file ROLE_1 DOC_NEW
-./rep_add_permission user2_org1_session_file ROLE_1 SUBJECT_NEW
-./rep_add_permission user2_org1_session_file ROLE_1 SUBJECT_DOWN
-./rep_add_permission user2_org1_session_file ROLE_2 SUBJECT_UP
-./rep_add_permission user2_org1_session_file ROLE_2 SUBJECT_DOWN
-./rep_add_permission user2_org1_session_file ROLE_2 SUBJECT_NEW
-./rep_add_permission user2_org1_session_file ROLE_2 ROLE_ACL
-./rep_add_permission user2_org1_session_file ROLE_3 DOC_NEW
-./rep_add_permission user2_org1_session_file ROLE_3 ROLE_NEW
-./rep_add_permission user2_org1_session_file ROLE_3 ROLE_MOD
-
-./rep_add_permission user9_org2_session_file ROLE_5 SUBJECT_UP
-./rep_add_permission user9_org2_session_file ROLE_5 SUBJECT_DOWN
-./rep_add_permission user9_org2_session_file ROLE_5 SUBJECT_NEW
-./rep_add_permission user9_org2_session_file ROLE_4 DOC_NEW
-./rep_add_permission user9_org2_session_file ROLE_4 ROLE_NEW
-./rep_add_permission user9_org2_session_file ROLE_4 ROLE_MOD
-./rep_add_permission user9_org2_session_file ROLE_6 SUBJECT_DOWN
-./rep_add_permission user9_org2_session_file ROLE_6 SUBJECT_NEW
-./rep_add_permission user9_org2_session_file ROLE_6 DOC_NEW
-
-
-# List role permissions
-./rep_list_role_permissions user2_org1_session_file ROLE_1
-./rep_list_role_permissions user2_org1_session_file ROLE_2
-./rep_list_role_permissions user2_org1_session_file ROLE_3
-
-./rep_list_role_permissions user9_org2_session_file ROLE_4
-./rep_list_role_permissions user9_org2_session_file ROLE_5
-./rep_list_role_permissions user9_org2_session_file ROLE_6
-
-# ROLE_1: [SUBJECT_DOWN, SUBJECT_NEW, DOC_NEW]
-# ROLE_2: [SUBJECT_UP, SUBJECT_DOWN, SUBJECT_NEW, ROLE_ACL]
-# ROLE_3: [DOC_NEW, ROLE_NEW, ROLE_MOD]
-
-# ROLE_4: [DOC_NEW, ROLE_NEW, ROLE_MOD]
-# ROLE_5: [SUBJECT_UP, SUBJECT_DOWN, SUBJECT_NEW]
-# ROLE_6: [SUBJECT_DOWN, SUBJECT_NEW, DOC_NEW]
-
-
-# Add subjects to roles
-./rep_list_subject_roles user1_org1_session_file user5
-./rep_add_permission user2_org1_session_file ROLE_1 user5
-./rep_list_subject_roles user1_org1_session_file user5
-./rep_list_roles user5_org1_session_file
-
-./rep_list_subject_roles user6_org2_session_file user8
-./rep_add_permission user9_org2_session_file ROLE_5 user8
-./rep_list_subject_roles user6_org2_session_file user8
-./rep_list_roles user8_org2_session_file
-
-
-# Remove subjects from roles
-./rep_remove_permission user2_org1_session_file ROLE_1 user5
-./rep_list_subject_roles user1_org1_session_file user5
-./rep_list_roles user5_org1_session_file
-
-./rep_remove_permission user9_org2_session_file ROLE_5 user8
-./rep_list_subject_roles user6_org2_session_file user8
-./rep_list_roles user8_org2_session_file
-
-
-# Remove permissions from roles
-./rep_remove_permission user2_org1_session_file ROLE_3 DOC_NEW
-./rep_list_role_permissions user2_org1_session_file ROLE_3
-
-./rep_remove_permission user9_org2_session_file ROLE_6 SUBJECT_DOWN
-./rep_list_role_permissions user9_org2_session_file ROLE_6
-
-# ROLE_3: [ROLE_NEW, ROLE_MOD]
-# ROLE_6: [DOC_READ, DOC_NEW]
-
-
-# Add document permissions to roles
-./rep_acl_doc user1_org1_session_file doc1 + ROLE_1 DOC_ACL
-./rep_acl_doc user1_org1_session_file doc2 + ROLE_1 DOC_READ
-./rep_acl_doc user1_org1_session_file doc3 + ROLE_1 DOC_DELETE
-./rep_acl_doc user1_org1_session_file doc4 + ROLE_2 DOC_ACL
-./rep_acl_doc user1_org1_session_file doc5 + ROLE_2 DOC_READ
-./rep_acl_doc user1_org1_session_file doc1 + ROLE_2 DOC_DELETE
-./rep_acl_doc user1_org1_session_file doc2 + ROLE_3 DOC_ACL
-./rep_acl_doc user1_org1_session_file doc3 + ROLE_3 DOC_READ
-./rep_acl_doc user1_org1_session_file doc4 + ROLE_3 DOC_DELETE
-
-./rep_acl_doc user6_org2_session_file doc6 + ROLE_4 DOC_ACL
-./rep_acl_doc user6_org2_session_file doc7 + ROLE_4 DOC_READ
-./rep_acl_doc user6_org2_session_file doc8 + ROLE_4 DOC_DELETE
-./rep_acl_doc user6_org2_session_file doc9 + ROLE_5 DOC_ACL
-./rep_acl_doc user6_org2_session_file doc10 + ROLE_5 DOC_READ
-./rep_acl_doc user6_org2_session_file doc6 + ROLE_5 DOC_DELETE
-./rep_acl_doc user6_org2_session_file doc7 + ROLE_6 DOC_ACL
-./rep_acl_doc user6_org2_session_file doc8 + ROLE_6 DOC_READ
-./rep_acl_doc user6_org2_session_file doc9 + ROLE_6 DOC_DELETE
-
-
-# Remove document permissions from roles
-./rep_acl_doc user1_org1_session_file doc1 - ROLE_1 DOC_ACL
-./rep_acl_doc user1_org1_session_file doc5 - ROLE_2 DOC_READ
-./rep_acl_doc user1_org1_session_file doc4 - ROLE_3 DOC_DELETE
-
-./rep_acl_doc user6_org2_session_file doc6 - ROLE_4 DOC_ACL
-./rep_acl_doc user6_org2_session_file doc10 - ROLE_5 DOC_READ
-./rep_acl_doc user6_org2_session_file doc9 - ROLE_6 DOC_DELETE
-
-
-# List permissions per role
-
-## Organization permissions
+# List the roles which have a permission
 ./rep_list_permission_roles user1_org1_session_file ROLE_ACL
-./rep_list_permission_roles user1_org1_session_file ROLE_MOD
-./rep_list_permission_roles user1_org1_session_file SUBJECT_NEW
-./rep_list_permission_roles user1_org1_session_file DOC_NEW
 
-./rep_list_permission_roles user6_org2_session_file DOC_NEW
-./rep_list_permission_roles user6_org2_session_file ROLE_NEW
-./rep_list_permission_roles user6_org2_session_file ROLE_MOD
-./rep_list_permission_roles user6_org2_session_file SUBJECT_DOWN
+# Suspend subject
+./rep_suspend_subject user1_org1_session_file user2
 
-## Document permissions
-./rep_list_permission_roles user1_org1_session_file DOC_ACL
-./rep_list_permission_roles user1_org1_session_file DOC_READ
-./rep_list_permission_roles user1_org1_session_file DOC_DELETE
+# User 2 attempts actions while suspended
+./rep_add_doc user2_org1_session_file doc1 file1.txt
+./rep_create_session org1 user2 456 user2_cred_file user2_org1_session_file
 
-./rep_list_permission_roles user6_org2_session_file DOC_ACL
-./rep_list_permission_roles user6_org2_session_file DOC_READ
-./rep_list_permission_roles user6_org2_session_file DOC_DELETE
+# Activate subject
+./rep_activate_subject user1_org1_session_file user2
 
-# --- End of script --- #
+# User 2 attempts actions while already activated
+./rep_add_doc user2_org1_session_file doc1 file1.txt
+
+# Document metadata and access
+./rep_get_doc_metadata user1_org1_session_file doc1 # Should fail, only the owner can access
+./rep_get_doc_metadata user2_org1_session_file doc1 
+# ./rep_get_file org1_a2e86de4670ad42936825ce9eb9fac5b2c3a1f14a186a4413d3068c1f7ab5d5b doc1_encrypted
+# ./rep_decrypt_file doc1_encrypted user2_org1/doc1_metadata.json
+./rep_get_doc_file user2_org1_session_file doc1
+./rep_list_docs user2_org1_session_file
+
+# Add document ACL
+./rep_acl_doc user2_org1_session_file doc1 - ROLE_2 DOC_ACL     # Should fail, this role is the only one with DOC_ACL
+./rep_acl_doc user2_org1_session_file doc1 + Manager DOC_READ   
+./rep_get_doc_metadata user1_org1_session_file doc1 # Can read
+
+./rep_acl_doc user1_org1_session_file doc1 - Manager DOC_READ  # Does not have acl permission
+./rep_get_doc_metadata user1_org1_session_file doc1 # Should still be able to read
+
+
+# Delete document (without permission)
+./rep_delete_doc user1_org1_session_file doc1 # Should fail, only the owner can delete
+./rep_acl_doc user2_org1_session_file doc1 + Manager DOC_DELETE  
+./rep_delete_doc user1_org1_session_file doc1 
+./rep_get_doc_metadata user1_org1_session_file doc1 
+
+#---
+# # Suspend and reactivate roles
+./rep_add_permission user1_org1_session_file ROLE_2 user1
+./rep_assume_role user1_org1_session_file ROLE_2     
+./rep_suspend_role user1_org1_session_file ROLE_2
+./rep_list_roles user1_org1_session_file                # Should not list ROLE_2
+./rep_list_roles user2_org1_session_file                # Should not list ROLE_2
+./rep_assume_role user2_org1_session_file ROLE_2        # Should fail
+./rep_reactivate_role user1_org1_session_file ROLE_2
+./rep_assume_role user2_org1_session_file ROLE_2     
+./rep_add_doc user2_org1_session_file doc2 file2.txt 
+
+
+./rep_remove_permission user1_org1_session_file ROLE_2 user2
+./rep_add_doc user2_org1_session_file doc3 file3.txt  # Should fail
+
+
+# # Drop and assume roles
+./rep_add_permission user1_org1_session_file ROLE_1 SUBJECT_DOWN
+
+./rep_drop_role user1_org1_session_file Manager
+./rep_assume_role user1_org1_session_file ROLE_1
+./rep_suspend_subject user1_org1_session_file user1 # Should fail because can't suspend a manager
+
+# Manage permissions
+./rep_assume_role user1_org1_session_file Manager
+./rep_remove_permission user2_org1_session_file ROLE_1 user1 # Should fail because doesn't have ROLE_MOD permission
+./rep_remove_permission user1_org1_session_file ROLE_1 user1
+
+./rep_remove_permission user1_org1_session_file Manager ROLE_ACL # Should fail
+./rep_remove_permission user1_org1_session_file Manager user1    # Should fail
+
+./rep_add_permission user1_org1_session_file Manager user2
+
+./rep_assume_role user2_org1_session_file Manager
+./rep_remove_permission user2_org1_session_file Manager user1
+
+./rep_add_doc user2_org1_session_file doc3 file3.txt
+./rep_acl_doc user2_org1_session_file doc3 + Manager DOC_ACL
+./rep_acl_doc user2_org1_session_file doc3 + Manager DOC_READ
+./rep_acl_doc user2_org1_session_file doc3 + Manager DOC_DELETE
+
+
+# List permissions of a role
+./rep_list_role_permissions user2_org1_session_file Manager
